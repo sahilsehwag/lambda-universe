b3c6cf205b13bbb0638d1a4f9c774b1927fe97f7
sahilsehwag <sehwagsahil002@gmail.com>
2020-12-15 14:47:11 +0000

    Implemented pair data-structure, pair, fst and snd operations

---
+++ pair.js
@@ -0,0 +1,10 @@
+import { K, KI, V } from './combinators'
+// import { succ } from './numbers'
+
+export const pair = V
+
+export const fst = p => p(K)
+
+export const snd = p => p(KI)
+
+// export const phi = p => pair(snd(p))(succ(snd(p)))
