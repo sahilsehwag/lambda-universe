4cf71045f106bf51a12f48c2bd7f5548335356ca
sahilsehwag <sehwagsahil002@gmail.com>
2020-12-15 18:12:24 +0000

    Refactored scratch.js

--- scratch.js
+++ scratch.js
@@ -1,184 +1,98 @@
-//variables
-//function-definition
-//function-application
 
-//identity-combinator
-const I = a => a
-
-//self-application-combinator
-//mockingbird-combinator
-const M = f => f(f)
-
-//first-combinator
-//kestrel-combinator
-const K = a => b => a
-
-//second-combinator
-//kite-combinator
-// const KI = K(I)
-const KI = a => b => b
-
-//application-combinator
-//identity-removed-combinator (I*)
-const A = f => a => f(a)
-const I1 = A
-
-//application-combinator(inverted)
-//thrush-combinator
-const T = a => f => f(a)
+export const M = f => f(f)
+export const K = a => _ => a
+export const KI = _ => b => b
+export const A = f => a => f(a)
+export const I1 = A
+export const T = a => f => f(a)
+export const I2 = f => a => b => f(a)(b)
+export const C = f => a => b => f(b)(a)
+export const V = a => b => f => f(a)(b)
+export const F = a => b => f => f(b)(a)
+export const B = f => g => a => f(g(a))
+export const B1 = f => g => a => b => f(g(a)(b))
+export const B2 = f => g => a => b => c => f(g(a)(b)(c))
+export const B3 = f => g => h => a => f(g(h(a)))
+export const S = f => g => a => f(a)(g(a))
 
 // //warbler-combinator
 // const W = f => a => f(a)(a)
 // //inverse-warbler
 // const W1 = a => f => f(a)(a)
 
-
-//identity-twice-removed-combinator
-const I2 = f => a => b => f(a)(b)
-//cardinal-combinator
-const C = f => a => b => f(b)(a)
-
-//argument-first composition-combinator
-//vireo-combinator
-const V = a => b => f => f(a)(b)
-//argument-first composition-combinator(reversed-arguments)
-//finch-combinator
-const F = a => b => f => f(b)(a)
-
-//composition-combinator (1* <- 1*)
-//bluebird-combinator
-const B = f => g => a => f(g(a))
-
-//composition-combinator(1* <- 2*)
-//blackbird-combinator
-const B1 = f => g => a => b => f(g(a)(b))
-
-//composition-combinator(1* <- 3*)
-//bunting-combinator
-const B2 = f => g => a => b => c => f(g(a)(b)(c))
-
-//composition-combinator(1* <- 3*)
-//becard-combinator
-const B3 = f => g => h => a => f(g(h(a)))
-
-
-
 //owl-combinator
 const O = a => b => b(a(b))
 
-//starling-combinator
-const S = a => b => c => a(c)(b)(c)
-
 //y-combinator
-// const Y = f => (x => f(x(x)))(x => f(x(x)))
+// const Y = f => f(f)
 // const Z = f => (x => f(x(x)))(x => f(x(x)))
 
 
 //booleans
-const bool = V('true')('false')
-const True = K
-const False = KI
+export const toJsBool = V(true)(false)
+export const True = K
+export const False = KI
+
+//boolean-operations
+export const not = C
+export const or = M
+export const and = a => b => a(b)(a)
+
+export const xor = a => b => a(not(b))(b)
+export const nand = B1(not)(and)
+export const nor = B1(not)(or)
+export const xnor = B1(not)(xor)
+export const beq = xnor
+
+export const imp = a => b => a(b)(not(a))
+export const convimp = a => b => b(a)(not(b))
 
 
 //numbers(church-numerals)
-const n0 = f => a => a
-const n1 = f => a => f(a)
-const number = n => n(a => a + 1)(0)
+export const toJsNumber = n => n(a => a + 1)(0)
 
-//peano-numbers
-const succ = n => f => a => f(n(f)(a))
+export const n0 = f => a => a
+export const n1 = f => a => f(a)
+export const succ = n => f => a => f(n(f)(a))
 
-const n2 = succ(n1)
-const n3 = succ(n2)
-const n4 = succ(n3)
-const n5 = succ(n4)
+export const n2 = succ(n1)
+export const n3 = succ(n2)
+export const n4 = succ(n3)
+export const n5 = succ(n4)
+export const n6 = succ(n5)
+export const n7 = succ(n6)
+export const n8 = succ(n7)
+export const n9 = succ(n8)
+export const n10 = succ(n9)
 
+export const is0 = n => n(K(False))(True)
 
-//character
-//string
+//arithematic-operations
+export const pred = n => fst(n(phi)(pair(n0)(n0)))
 
+export const add = n => m => n(succ)(m)
+export const mul = n => m => n(add(m))(n0)
+export const exp = n => m => m(mul(n))(n1)
 
-//data-structures
+export const sub = n => m => m(pred)(n)
+// export const div = n => m => //TODO
+// export const log = n => m => //TODO
+
+//arithematic-boolean-operations
+export const eq = n => m => is0(sub(n)(m))
+export const neq = B1(not)(eq)
+// export const lt =
+// export const gt =
+// export const lte = and(lt)
+// export const gte = and()
+
 //pair
 const pair = V
 const fst = p => p(K)
 const snd = p => p(KI)
 const phi = p => pair(snd(p))(succ(snd(p)))
-// const fst = T(K)
-// const snd = T(KI)
-
-//list
-//stack
-//queue
-//tree
-//graph
 
 
-//boolean-operations
-//intensional|extensional equality
-// const not = F(True)(False)
-const not = C
-
-// const or = a => b => a(a)(b)
-const or = M
-
-// const and = a => b => a(b)(a)
-const and = a => b => a(b)(a)
-
-// const xor = a => b => or(and(a)(not(b)))(and(not(a))(b))
-const xor = a => b => a(not(b))(b)
-const nand = B1(not)(and)
-const nor = B1(not)(or)
-const xnor = B1(not)(xor)
-const beq = xnor
-
-const imp = a => b => a(b)(not(a))
-const convimp = a => b => b(a)(not(b))
-
-
-//flow-control
-// const ifElse = B
-// console.log()
-
-
-//arithematic-operations
-const pred = n => fst(n(phi)(pair(n0)(n0)))
-const inc = succ
-const dec = pred
-
-const add = n => m => n(succ)(m)
-const mul = n => m => n(add(m))(n0)
-	//alpha-equivalence = equivalent function with just different argument-names
-	// const mul = n => m => B(n)(m)
-	// const mul = B
-const exp = n => m => m(mul(n))(n1)
-
-const sub = n => m => m(pred)(n)
-// const div = n => m => //TODO
-// const log = n => m => //TODO
-
-const is0 = n => n(K(False))(True)
-
-//arithematic-boolean-operations
-const eq = n => m => is0(sub(n)(m))
-const neq = B1(not)(eq)
-// const lt =
-// const gt =
-// const lte = and(lt)
-// const gte = and()
-
-
-
-
-
-
-
-
-//variables
-//function-definition
-//function-application
-
-//function-uses = value(input|output)|application
 
 // // 1-arguments (identity)(self-application)
 // // const I = a => a
@@ -221,6 +135,7 @@ const neq = B1(not)(eq)
 // 	// const Q4 = a => b => c => c(b(a))
 
 
+// RANDOM
 // // 4-arguments
 // // const _  = f => a => b => c => f(a)(b)(c)
 // const B1 = f => g => a => b => f(g(a)(b))
@@ -237,4 +152,12 @@ const neq = B1(not)(eq)
 // const D1 = a => b => c => d => e => a(b)(c)(d(e))
 // const D2 = a => b => c => d => e => a(b(c))(d(e))
 
-// // const S = a => b => c => //TODO
+// const E = f => a => g => b => c => f(a)(g(b)(c))
+// const BE = f => g => a => b => h => c => d => f(g(a)(b))(h(c)(d))
+
+// const GF = f => g => a => b => f(b)(g(a))
+// const H = f => a => b => f(a)(b)(a)
+// const J = f => a => b => c => f(a)(f(c)(b))
+// const L = f => g => f(g(g))
+// const M2 = f => g => f(g)(f(g))
+// const U = a => b => b(a(a)(b))
