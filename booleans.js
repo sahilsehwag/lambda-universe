4c84b7528d5f09aa96e5421d75cd2c2f39564604
sahilsehwag <sehwagsahil002@gmail.com>
2020-12-15 18:02:29 +0000

    Added church-booleans and their operations

---
+++ booleans.js
@@ -0,0 +1,19 @@
+import { K, KI, B1, C, M } from './combinators'
+
+export const True = K
+export const False = KI
+
+
+export const not = C
+export const or = M
+export const and = a => b => a(b)(a)
+
+
+export const xor = a => b => a(not(b))(b)
+export const nand = B1(not)(and)
+export const nor = B1(not)(or)
+export const xnor = B1(not)(xor)
+export const beq = xnor
+
+export const imp = a => b => a(b)(not(a))
+export const convimp = a => b => b(a)(not(b))
